319cc2965a053bfc452bc860a5c1051ee9515f28
Steve Golden <sgolden2525@outlook.com>
2020-05-09 19:05:16 -0500

    Started project
    Got boilerplate code
    Added axios and http-server
    In process of connecting to the weather app

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import { get } from 'axios';
+
+get('https://fcc-weather-api.glitch.me/')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
